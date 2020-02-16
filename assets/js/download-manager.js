eee3cc530ef182152248b653c90b42c30e8be426
Shresth Agrawal <shresthagrawal.31@gmail.com>
2020-02-16 11:53:17 +0530

    Add download-manager and integrate with the demo button

---
+++ assets/js/download-manager.js
@@ -0,0 +1,17 @@
+let latestVersion = '1.0.0'
+
+function redirectDownloadInk() {
+    switch(navigator.platform) {
+        case 'MacIntel':
+        window.location.replace(`https://github.com/ununu-p2p/ink-desktop/releases/download/v${ latestVersion }/ununu.Ink-${ latestVersion }.dmg`);
+        break;
+
+        case 'Win64':
+        window.location.replace(`https://github.com/ununu-p2p/ink-desktop/releases/download/v${ latestVersion }/ununu.Ink.Setup.${ latestVersion }.exe`);
+        break;
+
+        //TODO: Add for linux
+        default:
+        window.alert("The platform is not supported by INK, please try again on a Windows, Mac or Linux system!");
+    }
+}
