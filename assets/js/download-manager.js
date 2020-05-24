434178e8e56561586567b9ba0a8aedd29fee1fbe
Shresth Agrawal <shresthagrawal.31@gmail.com>
2020-05-24 12:58:39 +0200

    Redirect to the GCP latest build link

--- assets/js/download-manager.js
+++ assets/js/download-manager.js
@@ -1,23 +1,16 @@
-let latestVersion = "1.0.0";
 
 function redirectDownloadInk() {
   switch (navigator.platform) {
     case "MacIntel":
       window.location.replace(
-        `https://github.com/ununu-p2p/ink-desktop/releases/download/v${latestVersion}/ununu.Ink-${latestVersion}.dmg`
+        `https://storage.googleapis.com/ununu/ununu-latest.dmg`
       );
       break;
 
-    case "Win64":
-      window.location.replace(
-        `https://github.com/ununu-p2p/ink-desktop/releases/download/v${latestVersion}/ununu.Ink.Setup.${latestVersion}.exe`
-      );
-      break;
-
-    //TODO: Add for linux
+    //TODO: Add for windowns and linux
     default:
       window.alert(
-        "The platform is not supported by INK, please try again on a Windows, Mac or Linux system!"
+        "The platform is not supported by INK, please try again on a MacOS system!"
       );
   }
 }
