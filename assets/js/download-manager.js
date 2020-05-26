128500cc72dcf8596b2a29db024fee5cc60120c6
Jan Kaßel <jan@kassel.works>
2020-05-26 21:17:41 +0200

    feat: use permanent url for latest app version

--- assets/js/download-manager.js
+++ assets/js/download-manager.js
@@ -1,23 +1,11 @@
-let latestVersion = "1.0.0";
-
-function redirectDownloadInk() {
+function downloadRedirect() {
   switch (navigator.platform) {
     case "MacIntel":
-      window.location.replace(
-        `https://github.com/ununu-p2p/ink-desktop/releases/download/v${latestVersion}/ununu.Ink-${latestVersion}.dmg`
-      );
+      window.location = "https://ununu.io/download-latest.html";
       break;
 
-    case "Win64":
-      window.location.replace(
-        `https://github.com/ununu-p2p/ink-desktop/releases/download/v${latestVersion}/ununu.Ink.Setup.${latestVersion}.exe`
-      );
-      break;
-
-    //TODO: Add for linux
     default:
-      window.alert(
-        "The platform is not supported by INK, please try again on a Windows, Mac or Linux system!"
-      );
+      window.location = "https://ununu.io/";
+      break;
   }
 }
